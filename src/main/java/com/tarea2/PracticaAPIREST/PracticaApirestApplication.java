8afe43c85a6bf5676ef76d17af4b43278f888fd3
Andres Felipe Serrano Barrios <andresfserrano1@gmail.com>
2023-01-19 22:28:15 -0500

    Inicialización de práctica APIREST

---
+++ src/main/java/com/tarea2/PracticaAPIREST/PracticaApirestApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2.PracticaAPIREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea2/PracticaAPIREST/PracticaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2.PracticaAPIREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
